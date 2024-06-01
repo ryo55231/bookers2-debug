5937e4d70dc37af90d9bcb1fc82f5915e94182a7
EC2 Default User <ec2-user@ip-172-31-3-0.ap-northeast-1.compute.internal>
2024-06-01 03:58:25 +0000

    [Update] フォロー機能実装仮

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,20 @@ class User < ApplicationRecord
    has_many :book_comments, dependent: :destroy
    has_many :favorites, dependent: :destroy
    
+ #フォローしている
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  #フォローされてる
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  #フォローしている人(全体)
+  has_many :followings, through: :active_relationships, source: :followed
+  #フォローされている人（全体）
+  has_many :followers, through: :passive_relationships, source: :follower
     has_one_attached :profile_image
     
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  def relationship_by?(user)
+    relationship.exists?(user_id: user.id)
   end
 end

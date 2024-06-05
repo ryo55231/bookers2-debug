ca4fec48800b541fba84f94025bc038a5b8045b8
EC2 Default User <ec2-user@ip-172-31-3-0.ap-northeast-1.compute.internal>
2024-06-05 07:43:26 +0000

    [Update] いいね機能の非同期通信化仮

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,7 +10,7 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    #redirect_to request.referer
+    #redirect_to request.referer非同期通信化のため無効
   end
 
 end

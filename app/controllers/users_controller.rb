8f8e093b4d37d5849680537a27160c1449792832
EC2 Default User <ec2-user@ip-172-31-3-0.ap-northeast-1.compute.internal>
2024-08-20 07:11:31 +0000

    [update]応用課題7b完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+    #8/20投稿数の前日比 / 先週比を表示するための記述
+    @today_book = @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
+    #8/20ここまで
   end
 
   def index
--- app/models/book.rb
+++ app/models/book.rb
@@ -16,7 +16,12 @@ class Book < ApplicationRecord
     end
     image
   end
-  
+      #8/20投稿数の前日比 / 先週比を表示するための記述
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
+    #8/20ここまで
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end

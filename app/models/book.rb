9cb806c650a962dcdd32f8df2b9769dda30bee1f
ryo55231 <ryo1418105@gmail.com>
2024-08-21 09:54:26 +0000

    [update]応用課題8b完了

--- app/models/book.rb
+++ app/models/book.rb
@@ -18,6 +18,14 @@ class Book < ApplicationRecord
   end
       #8/20投稿数の前日比 / 先週比を表示するための記述
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  #8/21chart.jsを使って過去7日分の投稿数をグラフ化するための記述
+   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+   scope :created_2day_ago, -> { where(created_at: 2.day.ago.all_day) }
+   scope :created_3day_ago, -> { where(created_at: 3.day.ago.all_day) }
+   scope :created_4day_ago, -> { where(created_at: 4.day.ago.all_day) }
+   scope :created_5day_ago, -> { where(created_at: 5.day.ago.all_day) }
+   scope :created_6day_ago, -> { where(created_at: 6.day.ago.all_day) }
+  #8/21ここまで
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }

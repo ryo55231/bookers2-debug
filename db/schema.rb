f0fd29dc2197c88e7af9bfafa30c655f31ad1a53
ryo55231 <ryo1418105@gmail.com>
2024-09-05 07:21:32 +0000

    [update]通知機能のdb作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_17_025745) do
+ActiveRecord::Schema.define(version: 2024_09_05_071924) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -90,6 +90,17 @@ ActiveRecord::Schema.define(version: 2024_07_17_025745) do
     t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
+  create_table "notifications", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "notifiable_type", null: false
+    t.integer "notifiable_id", null: false
+    t.boolean "read", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["notifiable_type", "notifiable_id"], name: "index_notifications_on_notifiable"
+    t.index ["user_id"], name: "index_notifications_on_user_id"
+  end
+
   create_table "read_counts", force: :cascade do |t|
     t.integer "user_id"
     t.integer "book_id"
@@ -135,4 +146,5 @@ ActiveRecord::Schema.define(version: 2024_07_17_025745) do
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
   add_foreign_key "groups", "users"
+  add_foreign_key "notifications", "users"
 end

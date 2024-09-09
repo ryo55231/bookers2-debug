9140f24171d5aa746e84fb417daaf6014bf43b42
ryo55231 <ryo1418105@gmail.com>
2024-09-09 06:24:30 +0000

    [update]通知機能お気に入りコールバックmodel記述

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,8 +3,13 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :book
   #9/6通知機能との関連付け追記
-  has_one :notification, as: :notifiable, dependent: :destroy 
- #9/6ここまで
+  has_one :notification, as: :notifiable, dependent: :destroy
+  #9/6ここまで
 
+  #9/9通知機能　コールバックのための記述
+  after_create do
+    create_notification(user_id: book.user_id)
+  end
+  #9/9ここまで
   validates :user_id, uniqueness: {scope: :book_id}
 end

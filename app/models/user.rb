75fc5935f2acda4da6ae0ecd49a8d7ad24edb07d
ryo55231 <ryo1418105@gmail.com>
2024-09-11 10:11:52 +0000

    [update]通知機能お気に入りコールバックmodel記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,6 +49,7 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
+  
   # 6/2 ここからUser検索のモデルを追加 ※変数ではなく空のモデルとして入れるとのこと
     # 更にconsent methodの定義をつける→コントローラで変数として代入する
    def self.search_for(content, method)
